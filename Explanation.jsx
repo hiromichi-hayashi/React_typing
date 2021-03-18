9dec4af4bd8110f3b2b7576e928da9004ee38ee9
hiromichi-hayashi <75676003+hiromichi-hayashi@users.noreply.github.com>
2021-03-19 00:38:17 +0900

    pタグエラーの修正

---
+++ Explanation.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    content: {
+        fontSize: "16px",
+        fontFamily: "system-ui",
+    },
+});
+
+const Explantion = () => {
+    const classes = useStyles();
+    return (
+
+        <div className={classes.content}>
+            <p>出題される英単語をミス無くタイピングできるか挑戦しよう！</p>
+            <p> 制限時間は１分です</p>
+
+            <p>※画面をクリックしてからキーボード入力してください</p>
+        </div>
+
+    )
+}
+
+export default Explantion;
